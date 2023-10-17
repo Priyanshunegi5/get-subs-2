e77bb11864faa6b04b82d2b44843621ab76bd9c3
Raman Verma <devramanverma@gmail.com>
2023-10-17 12:11:31 +0530

    Added: data.js

---
+++ src/database/data.js
@@ -0,0 +1,24 @@
+/**
+ * An array of objects representing subscribed channel data.
+ *
+ * @type {Object[]}
+ * @property {string} name - The name of the subscriber.
+ * @property {string} subscribedChannel - The channel the subscriber is subscribed to.
+ */
+const data = [
+  {
+    name: "Jeread Krus",
+    subscribedChannel: "CNET",
+  },
+  {
+    name: "Lucifer",
+    subscribedChannel: "Sentex",
+  },
+  {
+    name: "John Doe",
+    subscribedChannel: "freeCodeCamp.org",
+  },
+];
+
+// Export the seeding data
+module.exports = data;
