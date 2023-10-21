5ad45d9c0b560346fd86601ffd9e3e53a17451be
Raman Verma <devramanverma@gmail.com>
2023-10-21 17:17:23 +0530

    Added: index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * ExpressJs server (Entry Point).
+ *
+ * Here we setup up a server using Express.js.
+ * Then connects it to a MongoDB database using Mongoose.
+ */
+
+/////////////////////////////////////////////////////////////////////////////
+// Here we import necessary modules required by server.
+/////////////////////////////////////////////////////////////////////////////
+// - Importing dotenv module
+const dotenv = require("dotenv");
+// - Importing custom app module
+const app = require("./src/app");
+// - Importing the custom database module
+const database = require("./src/database");
+
+// Get parsed env file
+const env = dotenv.config().parsed;
+
+/////////////////////////////////////////////////////////////////////////////
+// Connect to our database
+/////////////////////////////////////////////////////////////////////////////
+database.connect(
+  env.DATABASE_URL,
+  async (client) => {
+    // Here we listen incomming
+    // request to our server application.
+    app.listen(env.SERVER_PORT, () => {
+      console.log(`Server is running at port:`, env.SERVER_PORT);
+    });
+  },
+  (err, db) => {
+    if (!err) {
+      // Database successfuly connected
+      console.debug("Database connected");
+      return;
+    }
+
+    // Incase: something went wrong during database connection
+    console.error("Database connection failed.");
+    console.log(!env.APP_DEBUG ? err : err.stack);
+
+    // Terminate the process synchronously
+    process.exit(1);
+  }
+);
